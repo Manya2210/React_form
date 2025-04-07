3a3941054c0d67952bf4a3a3d770b48c885107bf
Manya2210 <59112605+Manya2210@users.noreply.github.com>
2025-04-07 19:14:55 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -16,6 +16,35 @@ const db = mongoose.connection;
 db.on('error', (error) => console.error('Database Connection Error :', error));
 db.once('open', () => console.log('connected to database'));
 
+app.get('/', (req, res) => {
+    UserModel.find({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get('/getUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findById({id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id: id}, {
+        name: req.body.name, 
+        email: req.body.email, 
+        age: req.body.age})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.delete('/deleteUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+    .then(res => res.json(res))
+    .catch(err => res.json(err))
+})
 app.post("/createUser", (req, res) => {
     console.log("Received data:", req.body);
     UserModel.create(req.body)
